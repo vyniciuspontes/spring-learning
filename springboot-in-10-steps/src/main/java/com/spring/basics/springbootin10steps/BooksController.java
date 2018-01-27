64adbb960b2e24ca5cbe0922d5cba18ff630e55c
Vynicius Pontes <pontes.vynicius@gmail.com>
2018-01-27 20:53:38 -0200

    Simple springboot rest application using the hal browser, actuator and devtools

---
+++ springboot-in-10-steps/src/main/java/com/spring/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,17 @@
+package com.spring.basics.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks(){
+		
+		return Arrays.asList(new Book(1, "My Book 1.0", "Vynicius Pontes"));
+	}
+}
